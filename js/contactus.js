e8d0a18c77a9e36cc10a8f7603663a01cc66ebe0
Ashish Raina <ashish.raina@bookmyshow.com>
2019-07-08 17:00:37 +0530

    added basic structure for all pages

--- js/contactus.js
+++ js/contactus.js
@@ -52,3 +52,19 @@ console.log("submitting form");
 $( "form" ).submit();
 
 }
+
+$(".servicename ").hover(function(){
+    $(this).addClass(' animate rubberBand')
+})
+
+$(document).ready(function(){
+    $(".burger").click(function(){
+        $('.sidebar').show();
+        $('.overlay').show();
+    })
+
+    $(".overlay").click(function(){
+        $('.sidebar').hide();
+        $('.overlay').hide();
+    })
+});
