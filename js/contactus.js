4e9e3f8f942fffd27d12a29a5c85a62539b514fa
Ashish Raina <ashish.raina@bookmyshow.com>
2019-07-15 18:30:30 +0530

    added more pages

--- js/contactus.js
+++ js/contactus.js
@@ -8,49 +8,48 @@ var email = $("#email").val();
 var message = $("#message").val();
 
 if (name.length == 0) {
-    $('#head').text("* All fields are mandatory *"); // This Segment Displays The Validation Rule For All Fields
-    $("#name").focus();
-    return false;
+        $('#head').text("* All fields are mandatory *"); // This Segment Displays The Validation Rule For All Fields
+        $("#name").focus();
+        return false;
     }
     // Validating Name Field.
     else if (!name.match(name_regex)) {
-    $('#head').text("* For your name please use alphabets only *"); // This Segment Displays The Validation Rule For Name
-    $("#name").focus();
-    return false;
+        $('#head').text("* For your name please use alphabets only *"); // This Segment Displays The Validation Rule For Name
+        $("#name").focus();
+        return false;
     } else {
         $('#head').text("");  
 }
 
 if (email.length == 0) {
-    $('#head2').text("* All fields are mandatory *"); // This Segment Displays The Validation Rule For All Fields
-    $("#email").focus();
-    return false;
+        $('#head2').text("* All fields are mandatory *"); // This Segment Displays The Validation Rule For All Fields
+        $("#email").focus();
+        return false;
     }
     // Validating Name Field.
     else if (!email.match(pattern)) {
-    $('#head2').text("* email format is not correct *"); // This Segment Displays The Validation Rule For Name
-    $("#email").focus();
-    return false;
+        $('#head2').text("* email format is not correct *"); // This Segment Displays The Validation Rule For Name
+        $("#email").focus();
+        return false;
     } else {
         $('#head2').text(""); 
 }
 
 if (message.length == 0) {
-    $('#head3').text("* All fields are mandatory *"); // This Segment Displays The Validation Rule For All Fields
-    $("#email").focus();
-    return false;
+        $('#head3').text("* All fields are mandatory *"); // This Segment Displays The Validation Rule For All Fields
+        $("#email").focus();
+        return false;
     }
     // Validating Name Field.
     else if (!message.match(pattern2) || message.length == 0) {
-    $('#head3').text("* email format is not correct *"); // This Segment Displays The Validation Rule For Name
-    $("#email").focus();
-    return false;
+        $('#head3').text("* email format is not correct *"); // This Segment Displays The Validation Rule For Name
+        $("#email").focus();
+        return false;
     } else {
         $('#head3').text("");  
 }
 console.log("submitting form");
-$( "form" ).submit();
-
+    $( "form" ).submit();
 }
 
 $(".servicename ").hover(function(){
